397b88213cca6ca2ba584ac322f5ac43d574308a
Bogdan Lovrencic <loki3uzu@gmail.com>
2023-09-11 16:59:28 +0200

    Created and configured project, created model, view and controller for products entity

---
+++ Products/Models/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Products.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Category { get; set; }
+        public string Manufacturer { get; set; }
+        public string Supplier { get; set; }
+        [Required]
+        public double Price { get; set; }
+            
+    }
+}
